aafa5b6ba00ab00974031dbb33463b836840cd40
Leah <rhodycodes401@gmail.com>
2018-06-26 01:15:05 +0000

    parse nodes

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-//js for D3
+var width = 640,
+height = 480;
+
+var links = [
+    { source: 'Ba', target: 'La'},
+    { source: 'Ba', target: 'St'},
+    { source: 'La', target: 'St'},
+    { source: 'Ta', target: 'Ba'},
+    ];
+    
+var nodes = {};
+
+//parse links to nodes
+
+links.forEach(function(link) {
+    links.source = nodes[link.source] || 
+        (nodes[link.source] = {name: link.source});
+    links.target = nodes[link.target] || 
+        (nodes[link.target] = {name: link.target});
+})
